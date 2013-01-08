d4e0d34f17b5871c47365aa6dda07517361dec19
Israel Fermin Montilla <iferminm@gmail.com>
2013-01-07 23:21:00 -0430

    Started to code my test platform based on unittest

---
+++ runtests.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+
+## #############################################################
+## Unnamed Social Lib
+## Testing module
+## Coded by: Israel Fermín Montilla <ferminster@gmail.com>
+## Caracas - Venezuela (2013)
+## #############################################################
+from sys import argv
+import unittest
+
+def run_tests(test_directory='tests'):
+    """
+    Runs the tests contained on the specified
+    directory
+    """
+    tests = unittest.defaultTestLoader.discover(test_directory)
+    runner = unittest.TextTestRunner()
+    runner.run(tests)
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        run_tests()
+    else:
+        run_tests(argv[1])
