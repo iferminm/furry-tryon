2796633b25724410a5b8bda515a9789c8c66325b
Israel Fermin Montilla <iferminm@gmail.com>
2013-01-07 23:40:24 -0430

    Created the scopes file to hold the gmail permission scopes

---
+++ gmail/scopes.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+
+## #############################################################
+## Unnamed Social Lib
+## Testing module
+## Coded by: Israel Fermín Montilla <ferminster@gmail.com>
+## Caracas - Venezuela (2012)
+## #############################################################
+#TODO: Scopes as constants here
