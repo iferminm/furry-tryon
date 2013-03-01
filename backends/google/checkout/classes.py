f49a905c85e8f07c3b0dff54433190cf2ab750f3
Israel Fermín Montilla <iferminm@gmail.com>
2013-03-01 14:02:46 -0430

    Skelleton for wraping aroung google checkout API

---
+++ backends/google/checkout/classes.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+from __future__ import unicode_literals
+import simplejson
+import urllib
+import urllib2
+import urls
+
+
+class GoogleCheckoutConnection(object):
+    """
+    This class manages the interaction with
+    google checkout service
+    """
+    def __init__(self, merchant_id, test=False):
+        pass 
